3c1ba2a8e4cca23af288e1c8730fd0f4b40137cc
Andrew <andrewhubbard78@gmail.com>
2023-09-21 12:08:51 -0400

    Andrew

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Given the following code, change the code to implement:
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
+            //int answer = 4; //explicit typing
+            //string response; // explicit typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+
+
+
+
         }
     }
 }
